93bcb2a6448fa97850bb86640b3a549ba14a1465
Kyle Racki <kyleracki@gmail.com>
2016-06-05 23:14:06 -0300

    Trying out static values to get basics in place

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@
 // load JSCON of team members and project data and store in arrays
 
 var team = [
+
 	new TeamMember(
 		"Kyle",
 		"Racki",
@@ -13,7 +14,7 @@ var team = [
 		"Ricky",
 		"Ferris",
 		40
-	),
+	)
 ];
 
 var projects = [
@@ -26,7 +27,7 @@ var projects = [
 		"Saucer",
 		350,
 		new Date(2016, 7, 12)
-	),
+	)
 ];
 
 var weeks = [
@@ -35,14 +36,85 @@ var weeks = [
 
 var saucer = new Saucer(projects, team);
 
-// click team member in table > load team view, assign that member as currentMember
 
+// click team member in table > load team view, assign that member as currentMember
 
 // click project in table > load project view, assign that project as currentProject
 
 // run loadProjectView method on load, load first project in list
 
 // Create 6 weeks on initial page load, load sliders from JSON file
+
+// slider.oninput -> call the slide method
+
+var slider1 = document.getElementById("123");
+var slider2 = document.getElementById("456");
+
+var weekSum = 0;
+
+function calculateAmount(slider) {
+	var sliderValue = slider.value;
+	
+	var total = function() {
+		return weekSum + sliderValue;
+	}
+	
+	console.log(sliderValue);
+	
+	
+	$( "#tableTotal td.this_week" ).text(total);
+
+}
+
+slider1.oninput = function() {
+	calculateAmount(slider1);
+}
+
+/*
+slider1.onchange = function() {
+	
+}
+*/
+
+slider2.oninput = function() {
+	calculateAmount(slider2);
+} 
+
+
+var SaucerUI = {
+	loadProjectView : function() {
+		
+
+	},
+	loadTeamView : function() {
+		
+	},
+	displayWeeks : function() {
+		
+	},
+	loadDropDown : function() {
+		
+	},
+	loadMainColumn : function() {
+		
+	},
+	sliderHandler : function(sliderId) {
+		
+		var slider = document.getElementById(sliderId);
+	
+		slider.oninput() = function() {
+			this.changeVal();
+		}
+		
+		slider.onchange() = function() {
+			this.saveVal();
+		}
+		
+		// slider.onchange -> save the value of the input to JSON
+	}
+}
+
+
 
 // next button increments currentWeek and if at the end of weeks, creates a new week
 // back button decriments currentWeek
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -9,14 +9,27 @@ function Slider(id, project, teamMember, week) {
 }
 
 
-Slider.prototype.drag = function() {
+Slider.prototype.changeVal = function() {
 	
-	this.stepDown(1);
+	this.amount = this.value;
+	
+	$( "tableTotal td.this_week" ).html(this.amount);
+	
+	return this.amount;
+	
+	/*
+this.stepDown(1);
 	this.stepUp(1);
+*/
+	
 	// update amount by 1
 	// increase or decrease project hours across all team members hours for that project
 	// update team member hours for the week
 	// if project hours exceeds hours budgeted, throw an alert badge
 	// update week total in the footer
 	
+};
+
+Slider.prototype.saveVal = function() {
+	console.log(this.value);
 };
